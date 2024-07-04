1cda03f9eda5bbb097d626e098e6fc82a20e7c07
Dmitriy <160003751+Komanchero1@users.noreply.github.com>
2024-07-04 22:59:58 +0400

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,5 +1,22 @@
 package org.example;
 
-public class PhoneBook {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
+public class PhoneBook {
+    public Map<String, String> contactsByName;
+    public Map<String, String> contactsByNumber;
+    public SortedSet<String> allNames;
+
+    public PhoneBook() {
+        contactsByName = new HashMap<>();
+        contactsByNumber = new HashMap<>();
+        allNames = new TreeSet<>();
+    }
+
+public int add(String name, String number) {
+        return 0;
+}
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,15 @@
-public class PhoneBookTest {
+import org.example.PhoneBook;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Alice", "12345"));
+        assertEquals(-1, phoneBook.add("Alice", "54321")); // Повторяющееся имя
+        assertEquals(2, phoneBook.add("Bob", "98765"));
+        assertEquals(3, phoneBook.add("Charlie", "2468"));
+    }
 }
