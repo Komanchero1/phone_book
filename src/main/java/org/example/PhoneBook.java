de334e339268d31be78432fc94ccdbd810a81d10
Dmitriy <160003751+Komanchero1@users.noreply.github.com>
2024-07-04 23:39:18 +0400

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -6,17 +6,27 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class PhoneBook {
-    public Map<String, String> contactsByName;
-    public Map<String, String> contactsByNumber;
-    public SortedSet<String> allNames;
+    public Map<String, String> contactsByName;//мапа в качестве ключей имена
+    public Map<String, String> contactsByNumber;//мапа в качестве ключей телефоны
+    public SortedSet<String> allNames;//коллекция для сортировки контактов
 
+
+    //в конструкторе  инициализируются три коллекции, которые будут использоваться
+    // для хранения и поиска контактов в телефонной книге
     public PhoneBook() {
         contactsByName = new HashMap<>();
         contactsByNumber = new HashMap<>();
         allNames = new TreeSet<>();
     }
-
-public int add(String name, String number) {
-        return 0;
-}
+    //метод добавляет контакт в телефонную книгу, принимает два аргумента name и  number
+    public int add(String name, String number) {
+        if (!contactsByName.containsKey(name)) {//проверяем наличие имени в книге
+            contactsByName.put(name, number);//если нет добавляем в мапу в качестве ключа выступает имя
+            contactsByNumber.put(number, name);//в эту мапу добавляем в качестве ключа выступает номер
+            allNames.add(name);//добавляем контакт в коллекцию
+            return contactsByName.size();//возвращается количество контактов
+        } else {
+            return -1; // если имя существует возвращается -1 контакт не добавлен
+        }
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -4,12 +4,22 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class PhoneBookTest {
+
+
+    //тест проверяет, что при добавлении нового контакта в телефонную книгу,
+    // метод add() возвращает правильное значение, соответствующее количеству контактов
+    // в телефонной книге
     @Test
     public void testAdd() {
-        PhoneBook phoneBook = new PhoneBook();
+        PhoneBook phoneBook = new PhoneBook();//создается объект класса PhoneBook
+        //вызывается метод add с аргументами Alice и 12345 метод проверяет есть такое имя или нет ,
+        // если нет добавляет контакт и возвращает число контактов , ожидаемо 1
         assertEquals(1, phoneBook.add("Alice", "12345"));
+        //тоже что и первый раз с теми же аргументами ожидаемый возврат -1, контакт повторяется не добавлен
         assertEquals(-1, phoneBook.add("Alice", "54321")); // Повторяющееся имя
+        //тоже что и первый раз  только меняем аргументы ожидаемый возврат 2
         assertEquals(2, phoneBook.add("Bob", "98765"));
+        //тоже что и первый раз  только меняем аргументы ожидаемый возврат 3
         assertEquals(3, phoneBook.add("Charlie", "2468"));
     }
 }
